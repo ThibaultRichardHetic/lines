3e20d987131c652d2b00a24d582746e4634d0bec
Richard <thibault.richard2000@gmail.com>
2021-04-08 19:00:40 +0200

    :tada: init project

---
+++ src/components/03_organisms/Footer/Footer.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="container--footer">
+    <div class="footer__text">
+      <a href="https://fr.louisvuitton.com/fra-fr/homepage/" target="_blank">
+        <CpText type="small">Site Louis Vuitton</CpText>
+      </a>
+      <a href=https://www.richard-thibault.com/ target="_blank">
+        <CpText type="small">Mentions Légales</CpText>
+      </a>
+      <a href=https://www.richard-thibault.com/ target="_blank">
+        <CpText type="small">Cookies</CpText>
+      </a>
+    </div>
+    <div class="downloads">
+      <ButtonStore color="dark" brand="apple"/>
+      <ButtonStore color="dark" brand="android"/>
+    </div>
+  </div>
+</template>
+
+<script>
+import CpText from "@/components/01_atoms/CpText/CpText.vue";
+import ButtonStore from "@/components/02_molecules/ButtonStore/ButtonStore.vue";
+
+export default {
+  name: "Footer",
+  components: {
+    CpText,
+    ButtonStore
+  },
+  data() {
+    return {
+      text: "Louis vuitton test"
+    };
+  }
+};
+</script>
+
+<style lang="scss">
+@import "./Footer.scss";
+</style>
---
+++ src/views/Home/Home.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="home">
+    <Header/>
+    <Hero/>
+    <Articles />
+    <Media />
+    <Footer />
+  </div>
+</template>
+
+<script>
+
+import Header from "@/components/03_organisms/Header/Header.vue";
+import Hero from "@/components/03_organisms/Hero/Hero.vue";
+import Articles from "@/components/03_organisms/Articles/Articles.vue";
+import Media from "@/components/03_organisms/Media/Media.vue";
+import Footer from "@/components/03_organisms/Footer/Footer.vue";
+
+export default {
+  name: "Home",
+  components: {
+    Header,
+    Hero,
+    Articles,
+    Media,
+    Footer
+  },
+  data() {
+    return {
+      text: "Louis vuitton test"
+    };
+  }
+};
+</script>
+
+<style lang="scss">
+@import "./Home.scss"
+</style>
